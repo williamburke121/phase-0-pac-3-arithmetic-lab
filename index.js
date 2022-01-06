7c7e41188db3a213407b32b7c991930ca4a555de
William Warner Burke <williamburke121@gmail.com>
2022-01-05 21:01:33 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a = 1, b = 1){
+    return a + b
+
+}
+function subtract(a = 10, b = 5){
+    return a - b
+}
+function multiply(a = 5, b = 5){
+    return a * b
+}
+function divide(a = 10, b = 2){
+    return a / b
+}
+
+function increment(n){
+    return ++ (n)
+}
+function decrement(n){
+    return -- (n)
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
